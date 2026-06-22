5bcc9a1fcb0db3511aca79fee9c6e50a55dcd2a9
Shaun Levy <SLY@ndonline.ac.uk>
2026-06-22 10:53:48 +0100

    started added some db tests

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+# tests/test_config.py
+
+import pytest
+
+from app import create_app, db
+
+
+@pytest.fixture
+def app():
+    app = create_app()
+
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
--- tests/test_db.py
+++ tests/test_db.py
@@ -6,12 +6,21 @@ def test_db(app):
 
 def test_create_user(app):
     with app.app_context():
-        user = User(username="alice")
+        user = User(email="alice", password="test")
 
         db.session.add(user)
         db.session.commit()
 
-        result = User.query.filter_by(username="alice").first()
+        result = User.query.filter_by(email="alice").first()
 
         assert result is not None
-        assert result.username == "alice"
+        assert result.email == "alice"
+
+def test_delete_user(app):
+    assert True
+
+def test_change_email(app):
+    assert True
+
+def test_change_password(app):
+    assert True
